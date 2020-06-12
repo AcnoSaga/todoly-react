609b6fc5171d775c9c4d94c12d9b7bd9a237c16d
Aamish Ahmad Beg <AcnoSaga@Akhlaks-MacBook-Pro.local>
2020-06-12 18:38:16 +0530

    Updates

---
+++ precache-manifest.35ec87f3a1e0a17ab76599b25318bb7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95cdfbffda1839382f1d19dbf89e6755",
+    "url": "/todoly-react/index.html"
+  },
+  {
+    "revision": "4f5a5a1006f85072650c",
+    "url": "/todoly-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2d4acf4c80d9ded414a7",
+    "url": "/todoly-react/static/css/main.00d35f22.chunk.css"
+  },
+  {
+    "revision": "4f5a5a1006f85072650c",
+    "url": "/todoly-react/static/js/2.8761279e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/todoly-react/static/js/2.8761279e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d4acf4c80d9ded414a7",
+    "url": "/todoly-react/static/js/main.0a0c5e4d.chunk.js"
+  },
+  {
+    "revision": "8eb39d54d0fb20d70881",
+    "url": "/todoly-react/static/js/runtime-main.14fdf79d.js"
+  }
+]);
